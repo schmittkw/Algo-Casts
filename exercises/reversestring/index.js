b08c5ac2c1dd48d18551a88259b1cb7619d65f0e
Kevin Schmitt <schmittkw@yahoo.com>
2018-01-22 22:05:45 -0600

    algo casts 1.0

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    var i = 0;
+    var newStr = ""
+    while(i<(str.length)/2){
+        
+        i++;
+    }
+}
+
+module.exports = reverse;
