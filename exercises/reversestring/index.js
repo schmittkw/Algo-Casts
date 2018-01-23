06f14c6dd336d819b11685a06ba7be0bd5927113
Kevin Schmitt <schmittkw@yahoo.com>
2018-01-22 22:50:03 -0600

    second commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,17 +2,43 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
     var i = 0;
-    var newStr = ""
+    var temp;
+    str = str.split("");
     while(i<(str.length)/2){
-        
+        temp = str[i];
+        str[i] = str[str.length-1-i];
+        str[str.length-1-i] = temp;
         i++;
     }
+    str = str.join("")
+    return str;
 }
+
+
+
+// very simple solution but sometimes not allowed to use reverse()
+
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+// }
+
+
+
+// more complex solution using reduce
+
+// function reverse(str){
+//     debugger;
+//     return str.split('').reduce((reverse, character) => character + reverse, '');
+// }
+
+// reverse('abcd');
+
+// must call function when inspecting with debugger/node
 
 module.exports = reverse;
