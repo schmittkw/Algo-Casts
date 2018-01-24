499ab9c0685f720d66b05ee4b9f74253ecbaeb43
Kevin Schmitt <schmittkw@yahoo.com>
2018-01-23 23:29:28 -0600

    solved maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    var string = str.split("");
+    for(let char of string){
+        chars[char] = chars[char] + 1 || 1;
+    }
+    var maxNum = 0;
+    var maxLetter = "";
+    for(let char of string){
+        if(chars[char] > maxNum){
+            maxNum = chars[char];
+        }
+        continue;
+    }
+    for(let char of string){
+        if(chars[char]==maxNum){
+            maxLetter = char;
+        }
+        continue;
+    }
+    return maxLetter;
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -25,4 +25,14 @@ function reverseInt(n) {
     return parseInt(n.join(""))
 }
 
+
+// another solution
+
+// function reverseInt(n) {
+//     const reversed = n.toString().split("").reverse().join("");
+
+//     return parseInt(reversed) * Math.sign(n);
+// }
+
+
 module.exports = reverseInt;
