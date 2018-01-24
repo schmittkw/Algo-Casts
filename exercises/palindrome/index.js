9f265bd7086508b8f025496903fa6ce23f74a195
Kevin Schmitt <schmittkw@yahoo.com>
2018-01-23 22:12:56 -0600

    solved Palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    var reversed = str.split("").reverse().join("")
+    if(reversed == str){
+        return true
+    }
+    else{
+        return false
+    }
+}
 
 module.exports = palindrome;
